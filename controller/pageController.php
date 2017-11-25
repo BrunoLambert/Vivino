9322f00a6da85ebf9b9b78563fc99a799240bd4d
BrunoLambert <brunofsclambert@gmail.com>
2017-11-25 20:41:26 -0200

    Fix Login/Register

--- controller/pageController.php
+++ controller/pageController.php
@@ -1,9 +1,11 @@
 <?php
+define("ROOT", "Vivino"); #Nome da pasta do seu Projeto
+
 if (!isset($_SESSION)) session_start();
 
 if(isset($_GET['change'])){
 	$_SESSION['action'] = $_GET['change'];
 }
 
-header("Location: ../../Vivino");
+header("Location: ../../" . ROOT);
 ?>
--- controller/usuarioController.php
+++ controller/usuarioController.php
@@ -2,6 +2,7 @@
 if(!isset($_SESSION)) session_start();
 
 if ($_SESSION['controller'] == 'register') {
+
 	include("config/database.php");
 
 	$sql = "insert into users values (NULL, '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['login'] . "', '" . $_POST['email'] . "', '" . $_POST['password'] . "')";
@@ -10,18 +11,29 @@ if ($_SESSION['controller'] == 'register') {
 	$result = mysqli_query($db, $sql);
 
 	if ($result){
+
+		$sql = "select * from users where email = '" . $_POST['email'] . "' and pword = '" . $_POST['password'] . "'";
+		$result = mysqli_query($db, $sql);
+		$row = $result->fetch_array();
+
+		$_SESSION['user_id'] = $row['id'];
+		$_SESSION['user_firstName'] = $row['firstName'];
+		$_SESSION['user_lastName'] = $row['lastName'];
+		$_SESSION['user_email'] = $row['email'];
+		$_SESSION['user_login'] = $row['login'];
+
+		mysqli_close($db);
 		unset($_SESSION['error']);
-		$_SESSION['action'] = 'index';
+		header("Location: controller/pageController.php?change=index");
 
 	}else{
-		$_SESSION['action'] = 'register';
 		$_SESSION['error'] = $db->error;
+		mysqli_close($db);
+		header("Location: controller/pageController.php?change=register");
 	}
 
-	mysqli_close($db);
-	header("Location: ../../Vivino");
-
 }else if ($_SESSION['controller'] == 'login'){
+
 	include("config/database.php");
 	$sql = "select * from users where email = '" . $_POST['email'] . "' and pword = '" . $_POST['password'] . "'";
 	$result = mysqli_query($db, $sql);
@@ -36,22 +48,22 @@ if ($_SESSION['controller'] == 'register') {
 			$_SESSION['user_email'] = $row['email'];
 			$_SESSION['user_login'] = $row['login'];
 
-			$_SESSION['action'] = 'index';
 			unset($_SESSION['error']);
+			mysqli_close($db);
+			header("Location: controller/pageController.php?change=index");
 
 		}else{
 
-			$_SESSION['action'] = 'login';
 			$_SESSION['error'] = "Login Invalido!";
+			mysqli_close($db);
+			header("Location: controller/pageController.php?change=login");
 		}
 
 	}else{
-		$_SESSION['action'] = 'login';
 		$_SESSION['error'] = $db->error;
+		mysqli_close($db);
+		header("Location: controller/pageController.php?change=login");
 	}
-
-	mysqli_close($db);
-	header("Location: ../Vivino");
 
 	
 }else if ($_SESSION['controller'] == 'logout'){
@@ -62,7 +74,6 @@ if ($_SESSION['controller'] == 'register') {
 	unset($_SESSION['user_email']);
 	unset($_SESSION['user_login']);
 
-	$_SESSION['action'] = 'index';
-	header('Location: ../Vivino');
+	header("Location: controller/pageController.php?change=index");
 }
 ?>
